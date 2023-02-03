7b6d5a326f38e43cdd9f824c373d4c2548dd18d9
Mani1358 <mani621998@gmail.com>
2023-02-03 22:09:25 +0530

    Added:UC-3

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -5,6 +5,6 @@ public class AddressBookMain {
    public static void main(String[] args) {
         AddressBook addressBook = new AddressBook();
         addressBook.addContact();
-
+        addressBook.editContact();
     }
 }
