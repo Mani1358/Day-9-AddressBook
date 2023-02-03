4bf8ff9940a3ce68b8d9a07fdcf6918945b4c5ca
Mani1358 <mani621998@gmail.com>
2023-02-03 22:00:49 +0530

    Added:UC-1

--- AddressBook.java
+++ AddressBook.java
@@ -1,7 +1,42 @@
 package com.blz.day_9_assignment;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class AddressBook {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Address Book");
+    Contact person = new Contact();
+    ArrayList<Contact> people = new ArrayList<Contact>();
+    Scanner sc = new Scanner(System.in);
+
+    public void addContact() {
+
+        System.out.println("First Name :");
+        person.firstName = sc.nextLine();
+
+        System.out.println("Last Name :");
+        person.lastName = sc.nextLine();
+
+        System.out.println("Address :");
+        person.address = sc.nextLine();
+
+        System.out.println("City :");
+        person.city = sc.nextLine();
+
+        System.out.println("State :");
+        person.state = sc.nextLine();
+
+        System.out.println("Zip :");
+        person.zip = sc.nextLine();
+
+        System.out.println("Phone Number :");
+        person.phoneNumber = sc.nextLine();
+
+        System.out.println("Email :");
+        person.email = sc.nextLine();
+
+        people.add(person);
     }
+
 }
+
